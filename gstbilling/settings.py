fc21eebf66b90e8916a990e9b5941cb0b9554042
bondasairam <97938396+bondasairam@users.noreply.github.com>
2022-01-25 12:14:38 +0530

    Update settings.py

--- gstbilling/settings.py
+++ gstbilling/settings.py
@@ -80,15 +80,8 @@ WSGI_APPLICATION = 'gstbilling.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bitwitz',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS':{
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR/'db.sqlite3',
     }
 }
 # SOCIAL_AUTH_POSTGRES_JSONFIELD = True
